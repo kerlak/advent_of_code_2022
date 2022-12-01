847a107a07c6869cab1ed80ba1eb3ed7d3b9b3ed
Juan Carlos Pazos <jucapaman@gmail.com>
2022-12-01 22:59:30 +0100

    day 01

--- lib/advent_of_code2022.ex
+++ lib/advent_of_code2022.ex
@@ -3,16 +3,32 @@ defmodule AdventOfCode2022 do
   Documentation for `AdventOfCode2022`.
   """
 
-  @doc """
-  Hello world.
+  def day_01_part_01 do
+    get_sorted_elves_calories()
+    |> List.first()
+  end
 
-  ## Examples
+  def day_01_part_02 do
+    get_sorted_elves_calories()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
 
-      iex> AdventOfCode2022.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp get_sorted_elves_calories() do
+    "lib/files/elves_calories.txt"
+    |> File.read!()
+    |> String.split("\n\n")
+    |> Enum.map(fn calories_str_list ->
+      calories_str_list
+      |> String.split("\n")
+      |> Enum.reduce(0, fn calories_str, total_calories ->
+        case Integer.parse(calories_str) do
+          {calories_val, ""} -> calories_val + total_calories
+          _ -> total_calories
+        end
+      end)
+    end)
+    |> Enum.sort()
+    |> Enum.reverse()
   end
 end
